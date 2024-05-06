c4e96411a8f20d37afca38dff89ed99cb6de9531
Sunny Panchal <sunnypanchalin@gmail.com>
2024-05-06 17:26:05 -0400

    a

--- Car Loan - CreditWorthiness 2024/app.py
+++ Car Loan - CreditWorthiness 2024/app.py
@@ -1,8 +1,27 @@
+from flask import Flask, jsonify, request # type: ignore
 import pickle
-import pandas as pd
+import pandas as pd # type: ignore
+
+app = Flask(__name__)
+
+with open('model_and_mappings.pkl', 'rb') as file:
+    model, mappings = pickle.load(file)
+
+FEATURE_ORDER = [
+    "Occupation",
+    "Monthly Income",
+    "Credit Score",
+    "Years of Employment",
+    "Finance Status",
+    "Finance History",
+    "Number of Children"
+]
 
 def predict_with_mapping(input_data, model, mappings):
-    input_df = pd.DataFrame.from_dict(input_data, orient='index').T
+    # Ensure input data follows the predefined feature order
+    ordered_input = {feature: input_data[feature] for feature in FEATURE_ORDER}
+    
+    input_df = pd.DataFrame.from_dict(ordered_input, orient='index').T
 
     encoded_data = pd.DataFrame(index=input_df.index)
 
@@ -14,14 +33,11 @@ def predict_with_mapping(input_data, model, mappings):
 
     prediction = model.predict(encoded_data)
 
-    return prediction
+    return prediction[0]
 
-with open('model_and_mappings.pkl', 'rb') as file:
-    model, mappings = pickle.load(file)
-
-from flask import Flask, jsonify, request
-
-app = Flask(__name__)
+@app.route("/ping", methods=["GET"])
+def ping():
+    return {"message": "Pinging the NEWWWW model successful!!"}
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -33,7 +49,5 @@ def predict():
     elif prediction == 1:
         prediction = "Approved"
 
-    return jsonify({'prediction': prediction})
+    return {'prediction': prediction}
 
-if __name__ == '__main__':
-    app.run(debug=True)
